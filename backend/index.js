b09c795889f519dff636c4762bd3a44961f7fa72
tylerasa <sarpongasaresylvester@gmail.com>
2021-12-13 16:00:47 +0000

    backend commit

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const request = require("request");
+const cheerio = require("cheerio");
+
+async function getSite() {
+  let temp = [];
+  request(
+    "http://162.12.215.254/Data/Movies/Hollywood/2016_2017/",
+    (error, response, html) => {
+      if (!error && response.statusCode === 200) {
+        const $ = cheerio.load(html);
+        $("a").each(function () {
+          let text = $(this).text();
+          let link = $(this).attr("href");
+          temp.push({
+            movie: text,
+            link: link,
+          });
+        });
+        console.log(temp);
+      }
+    }
+  );
+}
+
+getSite();
