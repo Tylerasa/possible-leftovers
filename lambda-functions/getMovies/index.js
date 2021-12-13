292033dbb94ccff045e500f46dc388fef0790aac
tylerasa <sarpongasaresylvester@gmail.com>
2021-12-13 16:14:06 +0000

    getMovies fuctionalities added to lambda function

--- lambda-functions/getMovies/index.js
+++ lambda-functions/getMovies/index.js
@@ -1,3 +1,23 @@
-exports.handler = async(event, context)=>{
-    
-}
+const request = require("request");
+const cheerio = require("cheerio");
+
+exports.handler = async (event, context) => {
+  let temp = [];
+  request(
+    "http://162.12.215.254/Data/Movies/Hollywood/2016_2017/",
+    (error, response, html) => {
+      if (!error && response.statusCode === 200) {
+        const $ = cheerio.load(html);
+        $("a").each(function () {
+          let text = $(this).text();
+          let link = $(this).attr("href");
+          temp.push({
+            movie: text,
+            link: link,
+          });
+        });
+        console.log(temp);
+      }
+    }
+  );
+};
